8c2a0983ed553186a17062f81ab7e00f7a098221
Augusto Iryoda <iryodap@gmail.com>
2021-11-29 08:25:40 -0300

    :sparkles: feat: implements ProfileRepository

--- src/modules/user/repositories/IProfileRepository.ts
+++ src/modules/user/repositories/IProfileRepository.ts
@@ -1,6 +1,8 @@
 import IUpdateProfile from '@modules/user/dtos/profile/IUpdateProfile';
 import Profile from '../domain/Profile';
+import ICreateProfile from '../dtos/profile/ICreateProfile';
 
 export default interface IProfileRepository {
+  create(data: ICreateProfile): Promise<Profile>;
   update(id: string, data: IUpdateProfile): Promise<Profile>;
 }
--- src/modules/user/repositories/prisma/ProfileRepository.ts
+++ src/modules/user/repositories/prisma/ProfileRepository.ts
@@ -2,8 +2,20 @@ import prisma from '@shared/prisma/client';
 import Profile from '@modules/user/domain/Profile';
 import IUpdateProfile from '@modules/user/dtos/profile/IUpdateProfile';
 import IProfileRepository from '../IProfileRepository';
+import ICreateProfile from '@modules/user/dtos/profile/ICreateProfile';
 
 export default class ProfileRepository implements IProfileRepository {
+  public async create(data: ICreateProfile): Promise<Profile> {
+    const profile = await prisma.profile.create({
+      data: {
+        userId: data.userId,
+        about: data.about,
+      },
+    });
+
+    return profile;
+  }
+
   public async update(id: string, data: IUpdateProfile): Promise<Profile> {
     const updatedProfile = await prisma.profile.update({
       where: { id },
