6fe6efd9433348d66e575e7cad6540aec18c2533
Augusto Iryoda <iryodap@gmail.com>
2021-11-29 08:28:27 -0300

    :tada: add: InMemoryProfileRepository

--- src/modules/user/repositories/in-memory/InMemoryProfileRepository.ts
+++ src/modules/user/repositories/in-memory/InMemoryProfileRepository.ts
@@ -1,17 +1,27 @@
 import Profile from '@modules/user/domain/Profile';
+import ICreateProfile from '@modules/user/dtos/profile/ICreateProfile';
 import IUpdateProfile from '@modules/user/dtos/profile/IUpdateProfile';
 import IProfileRepository from '../IProfileRepository';
+import { v4 } from 'uuid';
 
 export default class InMemoryProfileRepository implements IProfileRepository {
   private repository: Profile[] = [];
 
-  public async create(data: Profile): Promise<Profile> {
-    this.repository.push(data);
-    return data;
+  public async create(data: ICreateProfile): Promise<Profile> {
+    const profile: Profile = {
+      id: v4(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      ...data,
+    };
+
+    this.repository.push(profile);
+
+    return profile;
   }
 
   public async update(id: string, data: IUpdateProfile): Promise<Profile> {
-    const findProfile = this.repository.find((profile) => profile.id === id);
+    const findProfile = this.repository.find((profile) => profile.id === id)!;
 
     const newProfile = Object.assign(findProfile, { ...data });
 
