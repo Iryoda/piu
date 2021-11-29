6f6ab45c4bcd7db23e3ea238fa468dc87c364e5f
Augusto Iryoda <iryodap@gmail.com>
2021-11-29 08:20:35 -0300

    :sparkles: feat: post entity

--- src/modules/post/domain/Post.ts
+++ src/modules/post/domain/Post.ts
@@ -1,6 +1,8 @@
 import Comment from '@modules/comment/domain/Comment';
+import User from '@modules/user/domain/User';
 import { Post as PrismaPost } from '@prisma/client';
 
 export default interface Post extends PrismaPost {
-  comments: Comment[];
+  user?: User;
+  comments?: Comment[];
 }
