2b702767e2f1a0f4c6c11b206923b9a2084caa87
Augusto Iryoda <iryodap@gmail.com>
2021-12-02 12:45:40 -0300

    :tada: add: register LikeRepostiory

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -4,8 +4,11 @@ import './providers';
 import UserRepository from '../../modules/user/repositories/prisma/UserRepository';
 import IUserRepository from '@modules/user/repositories/IUserRepository';
 
-import IProfileRepository from '@modules/user/repositories/in-memory/InMemoryProfileRepository';
 import ProfileRepository from '@modules/user/repositories/prisma/ProfileRepository';
+import IProfileRepository from '@modules/user/repositories/IProfileRepository';
+
+import ILikeRepository from '@modules/like/repositories/ILikeRepository';
+import LikeRepository from '@modules/like/repositories/prisma/LikeRepository';
 
 container.registerSingleton<IUserRepository>('UserRepository', UserRepository);
 
@@ -13,3 +16,5 @@ container.registerSingleton<IProfileRepository>(
   'ProfileRepository',
   ProfileRepository,
 );
+
+container.registerSingleton<ILikeRepository>('LikeRepository', LikeRepository);
