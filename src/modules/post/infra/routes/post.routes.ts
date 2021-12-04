4eba543e26d4fd50dc6fcc2e12dc4f0c329b2c1b
Augusto Iryoda <iryodap@gmail.com>
2021-12-04 09:26:19 -0300

    :sparkles: feat: findAllController with routes

--- src/modules/post/infra/routes/post.routes.ts
+++ src/modules/post/infra/routes/post.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import CreatePostController from '../http/controllers/CreatePostController';
+import FindAllController from '../http/controllers/FindAllController';
 
 const postRoutes = Router();
 
 const createPostController = new CreatePostController();
+const findAllController = new FindAllController();
 
 postRoutes.post('/', createPostController.handle);
+postRoutes.get('/', findAllController.handle);
 
 export default postRoutes;
