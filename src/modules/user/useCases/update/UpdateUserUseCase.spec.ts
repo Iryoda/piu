19a402908e20c7fb10820d6b040fa80fb6a50697
Augusto Iryoda <iryodap@gmail.com>
2021-12-04 22:36:04 -0300

    ✅ test: add passing test if a param in invalid

--- src/modules/user/useCases/update/UpdateUserUseCase.spec.ts
+++ src/modules/user/useCases/update/UpdateUserUseCase.spec.ts
@@ -1,6 +1,8 @@
 import User from '@modules/user/domain/User';
 import InMemoryUserRepository from '@modules/user/repositories/in-memory/InMemoryUserRepository';
 import HashProvider from '@shared/container/providers/HashProvider/implementations/HashProvider';
+import AppError from '@shared/errors';
+import ShouldBeUnique from '@shared/errors/ShouldBeUnique';
 import CreateUserUseCase from '../create/CreateUserUseCase';
 import UpdateUserUseCase from './UpdateUserUseCase';
 
@@ -35,5 +37,25 @@ describe('UpdateUserUseCase', () => {
     });
 
     expect(updatedUser.username).toEqual('any_username2');
+  });
+
+  it('Should throw error if a param are invalid', async () => {
+    const invalidData = { username: 'any_username2', userId: 'invalid_id' };
+
+    await expect(
+      updateUserUseCase.handle({
+        id: user.id,
+        data: invalidData,
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('Should throw error if update user if already booked username', async () => {
+    await expect(
+      updateUserUseCase.handle({
+        id: user.id,
+        data: { username: 'any_username' },
+      }),
+    ).rejects.toBeInstanceOf(ShouldBeUnique);
   });
 });
