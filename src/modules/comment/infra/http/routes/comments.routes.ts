2d57fa2133e05430434e516f300d44624f404979
Augusto Iryoda <iryodap@gmail.com>
2021-12-13 17:32:13 -0300

    :sparkles: feat: add routes and change name

---
+++ src/modules/comment/infra/http/routes/comments.routes.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+import CreateCommentOnPostController from '../controller/CreateCommentOnPostController';
+import CreateResponseController from '../controller/CreateResponseController';
+import DeleteCommentController from '../controller/DeleteCommentController';
+import UpdateCommentController from '../controller/UpdateCommentController';
+
+const commentRoutes = Router();
+
+const createCommentOnPostController = new CreateCommentOnPostController();
+const deleteCommentController = new DeleteCommentController();
+const createResponseController = new CreateResponseController();
+const updateCommentController = new UpdateCommentController();
+
+commentRoutes.post('/:postid', createCommentOnPostController.handle);
+commentRoutes.post('/on/:commentId', createResponseController.handle);
+commentRoutes.post('/delete/:commentId', deleteCommentController.handle);
+commentRoutes.patch('/update/:commentId', updateCommentController.handle);
+
+export default commentRoutes;
