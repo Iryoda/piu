65c32ccacda788af473efef363e324c4640abdf6
Augusto Iryoda <iryodap@gmail.com>
2021-12-13 17:29:33 -0300

    :bug: fix: postId wans´t comming from params

--- src/modules/comment/infra/http/controller/CreateCommentOnPostController.ts
+++ src/modules/comment/infra/http/controller/CreateCommentOnPostController.ts
@@ -4,13 +4,17 @@ import { container } from 'tsyringe';
 
 export default class CreateCommentOnPostController {
   public async handle(req: Request, res: Response): Promise<Response> {
+    const { postId } = req.params;
     const data = req.body;
 
     const createCommentOnPostUseCase = container.resolve(
       CreateCommentOnPostUseCase,
     );
 
-    const response = await createCommentOnPostUseCase.handle(data);
+    const response = await createCommentOnPostUseCase.handle({
+      postId,
+      ...data,
+    });
 
     return res.status(201).json(response);
   }
