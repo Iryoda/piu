5fe2487bd22e91319bf15b184adc2ef8ba166eb3
Augusto Iryoda <iryodap@gmail.com>
2021-12-13 17:32:46 -0300

    :sparkles: feat:  delete and update methods

--- src/modules/comment/repository/inMemory/InMemoryCommentRepository.ts
+++ src/modules/comment/repository/inMemory/InMemoryCommentRepository.ts
@@ -2,6 +2,7 @@ import Comment from '@modules/comment/domain/Comment';
 import ICreateCommentOnPost from '@modules/comment/dtos/ICreateCommentOnPost';
 import ICreateResponse from '@modules/comment/dtos/ICreateResponse';
 import ICommentRepository from '../ICommentRepository';
+import IUpdateComment from '@modules/comment/dtos/IUpdateComment';
 import { v4 } from 'uuid';
 
 export default class InMemoryCommentRepository implements ICommentRepository {
@@ -31,6 +32,28 @@ export default class InMemoryCommentRepository implements ICommentRepository {
     this.repository.push(comment);
 
     return comment;
+  }
+
+  public async delete(id: string): Promise<void> {
+    this.repository.find((comment) => comment.id !== id);
+  }
+
+  public async update(data: IUpdateComment): Promise<Comment> {
+    let newComment = {} as Comment;
+
+    const newRepository = this.repository.map((comment) => {
+      if (comment.id === data.commentId) {
+        newComment = Object.assign(comment, { content: data.content });
+
+        return newComment;
+      }
+
+      return comment;
+    });
+
+    this.repository = newRepository;
+
+    return newComment;
   }
 
   public async findById(id: string): Promise<Comment | null> {
--- src/modules/comment/repository/prisma/CommentsRepository.ts
+++ src/modules/comment/repository/prisma/CommentsRepository.ts
@@ -3,6 +3,7 @@ import Comment from '@modules/comment/domain/Comment';
 import ICreateCommentOnPost from '@modules/comment/dtos/ICreateCommentOnPost';
 import ICommentRepository from '../ICommentRepository';
 import ICreateResponse from '@modules/comment/dtos/ICreateResponse';
+import IUpdateComment from '@modules/comment/dtos/IUpdateComment';
 
 export default class CommentsRepository implements ICommentRepository {
   private repository = prisma.comment;
@@ -10,6 +11,19 @@ export default class CommentsRepository implements ICommentRepository {
   public async createOnPost(data: ICreateCommentOnPost): Promise<Comment> {
     const comment = await this.repository.create({ data: data });
     return comment;
+  }
+
+  public async update(data: IUpdateComment): Promise<Comment> {
+    const newComment = await this.repository.update({
+      where: { id: data.commentId },
+      data: { content: data.content },
+    });
+
+    return newComment;
+  }
+
+  public async delete(id: string): Promise<void> {
+    await this.repository.delete({ where: { id } });
   }
 
   public async createResponse(data: ICreateResponse): Promise<Comment> {
