609f7debc43dc2d78bd5187f5b06be84cc6489ec
Augusto Iryoda <iryodap@gmail.com>
2021-12-21 17:45:50 -0300

    :tada: add: mockResponse

---
+++ src/shared/mocks/ResponseMock.ts
@@ -0,0 +1,10 @@
+import { Response } from 'express';
+
+const mockResponse = (): Response => {
+  const res: Response = {} as Response;
+  res.status = jest.fn().mockReturnValue(res);
+  res.json = jest.fn().mockReturnValue(res);
+  return res;
+};
+
+export default mockResponse;
