e3b73d19d0ba520082db5d0ba34ff5c597240425
Augusto Iryoda <iryodap@gmail.com>
2022-01-08 20:46:54 -0300

    :construction: refactor: remove param validation responsability

--- src/modules/post/useCase/create/CreatePostUseCase.spec.ts
+++ src/modules/post/useCase/create/CreatePostUseCase.spec.ts
@@ -1,8 +1,6 @@
 import InMemoryPostRepository from '@modules/post/repository/inMemory/InMemoryPostRepository';
 import InMemoryUserRepository from '@modules/user/repositories/in-memory/InMemoryUserRepository';
 import CreatePostUseCase from './CreatePostUseCase';
-import InvalidParam from '@shared/errors/InvalidParamError';
-import MissingParam from '@shared/errors/MissingParam';
 import NotFound from '@shared/errors/NotFound';
 import User from '@modules/user/domain/User';
 
@@ -44,27 +42,5 @@ describe('CreatePostUseCase', () => {
         content: 'any_content',
       }),
     ).rejects.toBeInstanceOf(NotFound);
-  });
-
-  it('Should throw an erro if a invalid param is passed', async () => {
-    const data = {
-      userId: 'invalid_id',
-      content: 'any_content',
-      invalidParam: 'invalid_param',
-    };
-
-    await expect(createPostUseCase.handle(data)).rejects.toBeInstanceOf(
-      InvalidParam,
-    );
-  });
-
-  it('Should throw an erro if a missing a param ', async () => {
-    const data = {
-      userId: 'invalid_id',
-    };
-
-    await expect(createPostUseCase.handle(data as any)).rejects.toBeInstanceOf(
-      MissingParam,
-    );
   });
 });
--- src/modules/post/useCase/create/CreatePostUseCase.ts
+++ src/modules/post/useCase/create/CreatePostUseCase.ts
@@ -4,8 +4,6 @@ import IPostRepository from '@modules/post/repository/IPostRepository';
 import IUserRepository from '@modules/user/repositories/IUserRepository';
 import NotFound from '@shared/errors/NotFound';
 import ICreatePostDTO from '@modules/post/dtos/ICreatePostDTO';
-import InvalidParam from '@shared/errors/InvalidParamError';
-import MissingParam from '@shared/errors/MissingParam';
 
 @injectable()
 export default class CreatePostUseCase {
@@ -14,21 +12,8 @@ export default class CreatePostUseCase {
     @inject('UserRepository') private userRepository: IUserRepository,
   ) {}
 
-  private checkIfParamIsValid(data: ICreatePostDTO): void {
-    const validParam: Array<keyof ICreatePostDTO> = ['userId', 'content'];
-
-    validParam.map((param) => {
-      if (!data[param]) throw new MissingParam(param);
-    });
-
-    Object.keys(data).map((param) => {
-      if (!validParam.includes(param as keyof ICreatePostDTO))
-        throw new InvalidParam(param);
-    });
-  }
-
   public async handle(data: ICreatePostDTO): Promise<Post> {
-    this.checkIfParamIsValid(data);
+    await Post.create(data.content);
 
     const findUser = await this.userRepository.findOneById(data.userId);
 
