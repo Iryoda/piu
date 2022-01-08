1159fdd45a752afef10eaa2a18fd4726270a49e3
Augusto Iryoda <iryodap@gmail.com>
2022-01-08 20:47:49 -0300

    :construction: refactor: add params validation

--- src/modules/post/infra/http/controllers/CreatePostController.ts
+++ src/modules/post/infra/http/controllers/CreatePostController.ts
@@ -1,11 +1,28 @@
 import ICreatePostDTO from '@modules/post/dtos/ICreatePostDTO';
 import CreatePostUseCase from '@modules/post/useCase/create/CreatePostUseCase';
+import InvalidParam from '@shared/errors/InvalidParamError';
+import MissingParam from '@shared/errors/MissingParam';
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 export default class CreatePostController {
+  private checkIfParamIsValid(data: ICreatePostDTO): void {
+    const validParam: Array<keyof ICreatePostDTO> = ['userId', 'content'];
+
+    validParam.map((param) => {
+      if (!data[param]) throw new MissingParam(param);
+    });
+
+    Object.keys(data).map((param) => {
+      if (!validParam.includes(param as keyof ICreatePostDTO))
+        throw new InvalidParam(param);
+    });
+  }
+
   public async handle(req: Request, res: Response): Promise<Response> {
     const data: ICreatePostDTO = req.body;
+
+    this.checkIfParamIsValid(data);
 
     const createPostUseCase = container.resolve(CreatePostUseCase);
 
